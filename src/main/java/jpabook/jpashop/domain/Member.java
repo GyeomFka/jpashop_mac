449a38329dc5f0faffeb2ae15b654c8c27e64f50
GyeomFka <clazziquai01@gmail.com>
2021-12-25 11:37:18 +0900

    create entity class

---
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,31 @@
+package jpabook.jpashop.domain;
+
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Order;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Member {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    private String name;
+
+    @Embedded
+    private Address address;
+
+    @OneToMany(mappedBy = "member") //매핑 당하는 개체
+    private List<Order> orders = new ArrayList<>();
+
+}
