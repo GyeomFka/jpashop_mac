8a1db1f4ee10ca464da26bdd60755fa36eb282b6
GyeomFka <clazziquai01@gmail.com>
2021-12-26 15:14:33 +0900

    주문 도메인 테스트 개발

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,16 +1,20 @@
 package jpabook.jpashop.domain;
 
 import jpabook.jpashop.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
 
 import static javax.persistence.FetchType.LAZY;
+import static lombok.AccessLevel.PROTECTED;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor(access = PROTECTED)
 public class OrderItem {
 
     @Id
@@ -30,4 +34,29 @@ public class OrderItem {
 
     private int count; // 주문 수량
 
+//    protected OrderItem() {} -> lombok 처리
+
+    //생성 메서드
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+    }
+
+    //비즈니스 로직
+    public void cancle() {
+        getItem().addStock(count);
+    }
+
+    //조회로직
+    /*
+    * 주문상품 전체 가격 조회
+    * */
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
